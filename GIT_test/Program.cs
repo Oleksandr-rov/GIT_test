715b608fa07e3788ceb63eb525fbdb3140e0d0af
Oleksandr Rohan <aleksandr.rav@gmail.com>
2024-04-22 18:56:09 +0300

    Mastr, commit #2

--- GIT_test/Program.cs
+++ GIT_test/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Start GIT test!");
 Console.WriteLine("Master commit #1!");
+Console.WriteLine("Master commit #2!");
