1380be7a7b562c82c9e85347bc773c01ff7f5a50
Oleksandr Rohan <aleksandr.rav@gmail.com>
2024-04-28 21:31:11 +0300

    add class2 and f = 0.2

---
+++ GIT_test/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_test
+{
+    internal class Class2
+    {
+        float f = 0.2f;
+    }
+}
